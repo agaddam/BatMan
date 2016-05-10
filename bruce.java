52665798287a4ffd43f288ce11c449a8888a71d4
agaddam <abhi.gaddam1@gmail.com>
2016-05-10 14:54:33 -0500

    Update bruce.java

--- bruce.java
+++ bruce.java
@@ -1,5 +1,7 @@
 public class BruceWayne{
 	public static void main(String[] args){
 		system.out.println(“Bruce is back……!!!!”);
+	        system.out.println(“Github says welcome bruce…!!!!”);
+
 	}
 }
